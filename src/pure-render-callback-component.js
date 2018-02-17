c66659315266a1cc855b38873ef8f7e4e341dd9f
Ben Styles <enquiries@leeds-ebooks.co.uk>
2018-02-17 11:33:17 +0000

    add `props.render` (aka **render prop**) as optional alternative to `props.children` (aka **render callback**)

--- src/pure-render-callback-component.js
+++ src/pure-render-callback-component.js
@@ -5,12 +5,14 @@ import omit from 'lodash.omit'
 import shallowEqual from 'fbjs/lib/shallowEqual'
 
 export default class PureRenderCallbackComponent<
-  P: {children: Function, +extraProps?: Object},
+  P:
+    | {children: Function, +extraProps?: Object}
+    | {render: Function, +extraProps?: Object},
   S = *
 > extends Component<P, S> {
   shouldComponentUpdate(nextProps: P, nextState: S) {
     const {props, state} = this
-    const omitKeys = ['extraProps', 'children']
+    const omitKeys = ['extraProps', 'children', 'render']
     return (
       !shallowEqual(state, nextState) ||
       !shallowEqual(omit(props, omitKeys), omit(nextProps, omitKeys)) ||
